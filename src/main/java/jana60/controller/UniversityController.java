e0a18d68ccfb60e90c83fb169a7b5e39822b4ca3
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-07-15 15:41:46 +0200

    Effettuato navabar logo e navigzione interna sito con dipartimenti

--- src/main/java/jana60/controller/UniversityController.java
+++ src/main/java/jana60/controller/UniversityController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import jana60.model.University;
-import jana60.repository.UniversityRepository;
 
+import jana60.model.Degrees;
+import jana60.model.University;
+import jana60.repository.DegreesRepository;
+import jana60.repository.UniversityRepository;
 
 @Controller
 @RequestMapping("/")
@@ -17,12 +19,26 @@ public class UniversityController {
 	
 	@Autowired 
 	private UniversityRepository repo;
-
+	@Autowired
+	private DegreesRepository repo2;
+	
 	@GetMapping
+	public String fristpage() {
+		return "fristpage";
+	}
+
+	@GetMapping("/dipartimenti")
 	public String home(Model model) {
-		List<University> DepartmentsList =(List<University>)repo.findAll();
+		List<University> DepartmentsList =(List<University>)repo.findAllByOrderByName();
 		model.addAttribute("DepartmentsList" ,DepartmentsList);
 		return "home";
+	}
+	
+	@GetMapping("/degrees")
+	public String degrees(Model model) {
+		List<Degrees> DegreesList = (List<Degrees>)repo2.findAll();
+		model.addAttribute("DegreesList" ,DegreesList);
+		return "degrees";
 	}
 	
 }
--- src/main/java/jana60/repository/UniversityRepository.java
+++ src/main/java/jana60/repository/UniversityRepository.java
@@ -1,5 +1,7 @@
 package jana60.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import jana60.model.University;
 @Repository
 public interface UniversityRepository extends CrudRepository <University, Integer> {
 
-	
+	List<University> findAllByOrderByName();
 }
